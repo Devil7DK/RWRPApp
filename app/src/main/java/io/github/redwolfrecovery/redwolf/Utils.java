840fa2fa8849d10cbc7e83f8f436cc9791589145
Devil7DK <dineshthangavel47@gmail.com>
2018-04-13 14:24:32 +0530

    Add "Check for Updates"
    Step 1 : Add "incremental" attribute to "device" elements in XML file stored in remote and set its value to ro.build.incremental prop of latest recovery build
    Step 2 : Extract recovery image using dumpimage, Extract the image using unpackbootimg.
    Step 3 : Unpack ramdisk with gzip & cpio. Read ro.build.version.incremental from "default.prop"
    Step 4 : Compare values read from remote & recovery default.prop

--- app/src/main/java/io/github/redwolfrecovery/redwolf/Utils.java
+++ app/src/main/java/io/github/redwolfrecovery/redwolf/Utils.java
@@ -1,19 +1,36 @@
 package io.github.redwolfrecovery.redwolf;
 
 import android.app.ActivityManager;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Environment;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.text.DecimalFormat;
+import java.util.Properties;
 
 /**
  * Created by root on 3/1/18.
  */
 
 public class Utils {
+
+    static String appFileDirectory;
+    static String dumpimage_path;
+    static String unpackbootimg_path;
 
     public static long GetMemorySize(Context mContext) {
         ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
@@ -97,6 +114,143 @@ public class Utils {
             return context.getString(R.string.download_speed_kb, decimalFormat.format(kb));
         } else {
             return context.getString(R.string.download_speed_bytes, downloadedBytesPerSecond);
+        }
+    }
+
+    public static String getRecoveryIncrimentalVersion(Context context, ProgressDialog progressDialog){
+        progressDialog.setMessage(context.getString(R.string.preparing_executables));
+        PrepareExecutables(context);
+
+        File temp = new File(context.getCacheDir() + File.separator + "temp");
+        File tempImg = new File(temp.getAbsolutePath(), "recovery.img");
+        File ramdisk = new File(temp.getAbsolutePath(), "ramdisk.cpio.gz");
+        File propfile = new File(Environment.getExternalStorageDirectory() + File.separator + "recovery.prop");
+
+        try {
+            if(temp.exists()){
+                progressDialog.setMessage(context.getString(R.string.removing_temp));
+                temp.delete();
+            }else{temp.mkdirs();}
+
+            String line;
+            Process process = Runtime.getRuntime().exec("su");
+            OutputStream stdin = process.getOutputStream();
+            InputStream stderr = process.getErrorStream();
+            InputStream stdout = process.getInputStream();
+
+            String cmd1 = "cd "+ temp.getAbsolutePath() +"\n";
+            String cmd2 = dumpimage_path + " /dev/block/bootdevice/by-name/recovery " + tempImg.getAbsolutePath() + "\n";
+            String cmd3 = unpackbootimg_path + " --input " + tempImg.getAbsolutePath() + " --ramdisk " + ramdisk.getAbsolutePath() + "\n";
+            String cmd4 = "gzip -cd " + ramdisk.getAbsolutePath() + " | cpio -iv\n";
+            String cmd5 = "chmod -R 0777 " + temp.getAbsolutePath() + "\n";
+            String cmd6 = "rm -rf !(default.prop)" +"\n";
+            String cmd7 = "mv default.prop " + propfile.getAbsolutePath() + "\n";
+
+
+            progressDialog.setMessage(context.getString(R.string.dumping_recoveryimage));
+            stdin.write(cmd1.getBytes());
+            stdin.write(cmd2.getBytes());
+            progressDialog.setMessage(context.getString(R.string.unpacking_recoveryimage));
+            stdin.write(cmd3.getBytes());
+            progressDialog.setMessage(context.getString(R.string.extracting_ramdisk));
+            stdin.write(cmd4.getBytes());
+            stdin.write(cmd5.getBytes());
+            stdin.write(cmd6.getBytes());
+            stdin.write(cmd7.getBytes());
+
+            stdin.write("exit\n".getBytes());
+            stdin.flush();
+
+            stdin.close();
+
+            process.waitFor();
+            process.destroy();
+
+            progressDialog.setMessage(context.getString(R.string.reading_prop));
+            Properties prop = new Properties();
+            try{
+
+                prop.load(new FileInputStream(propfile.getAbsolutePath()));
+                String v1 = prop.getProperty("ro.build.version.incremental","");
+                //propfile.delete();
+                return v1;
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+
+        } catch (Exception ex) {
+        } finally {
+            if(temp.exists()){
+                if(progressDialog.isShowing())progressDialog.setMessage(context.getString(R.string.removing_temp));
+                temp.delete();
+            }
+        }
+            return "";
+    }
+
+    public static void PrepareExecutables(Context context){
+        try{
+            appFileDirectory = context.getFilesDir().getPath();
+            String dumpimage_name = "dump_image";
+            String unpackbootimg_name = "unpackbootimg";
+            dumpimage_path = appFileDirectory + File.separator + dumpimage_name;
+            unpackbootimg_path = appFileDirectory + File.separator + unpackbootimg_name;
+            copyAssets(dumpimage_name, dumpimage_path, context);
+            copyAssets(unpackbootimg_name, unpackbootimg_path, context);
+            (new File (dumpimage_path)).setExecutable(true);
+            (new File (unpackbootimg_path)).setExecutable(true);
+        }catch(Exception ex){ex.printStackTrace();}
+    }
+
+
+    public static String getArch(){
+        String deviceArch = android.os.Build.SUPPORTED_ABIS[0];
+        if(deviceArch.contains("armeabi")){
+            return "arm";
+        }else if(deviceArch.contains("arm64")){
+            return "arm64";
+        }else if(deviceArch.contains("x86_64")){
+            return "x86_64";
+        }else if(deviceArch.equals("x86")){
+            return "x86";
+        }
+        return "arm";
+    }
+
+    public static void copyAssets(String filename, String fullpath, Context context) {
+        AssetManager assetManager = context.getAssets();
+
+        InputStream in = null;
+        OutputStream out = null;
+
+        try {
+            in = assetManager.open(getArch() + "/" + filename);
+            File outFile = new File(fullpath);
+            out = new FileOutputStream(outFile);
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out = null;
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void copyFile(InputStream in, OutputStream out) throws IOException {
+        try {
+
+            try {
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+            } finally {
+                out.close();
+            }
+        } finally {
+            in.close();
         }
     }
 }
